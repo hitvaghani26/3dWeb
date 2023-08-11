488ff7b6205575b51e8f788a6e4e50a81c9c1813
Hit Vaghani <68742522+hitvaghani26@users.noreply.github.com>
2023-08-11 15:10:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Spline from '@splinetool/react-spline';
+import "./App.css"
+
+function App() {
+  return (
+    <>
+    <div className='main-section'>
+
+    <Spline className='robo' scene="https://prod.spline.design/33bIBPxqpjgxtAAH/scene.splinecode" />
+    <h1 className='title'>Made by Hit Vaghani</h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
